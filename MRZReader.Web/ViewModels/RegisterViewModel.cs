85c07590eb3bd8cfb1dfad35d5c38eea6dbdf502
Mostafa Mahgoub <mostafa.mahgoub@belgiantrain.be>
2019-09-12 23:38:20 +0200

    Adding register view model Adding register page Adding Account Controller -HTTPGet only

---
+++ MRZReader.Web/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MRZReader.Web.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email{ get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password{ get; set; }
+
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Passeord")]
+        [Compare("Password", ErrorMessage= "Password and confirmation password did not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
