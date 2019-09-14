fb86263a11c1c14972a106d00b4e7ea5de300b72
Mostafa Mahgoub <mostafa.mahgoub@belgiantrain.be>
2019-09-14 02:01:35 +0200

    Adding Document page

--- MRZReader.Web/Controllers/DocumentController.cs
+++ MRZReader.Web/Controllers/DocumentController.cs
@@ -1,21 +1,22 @@
-﻿//using Microsoft.AspNetCore.Mvc;
-//using MRZReader.Core;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using MRZReader.Core;
+using ReflectionIT.Mvc.Paging;
 
-//namespace MRZReader.Web.Controllers
-//{
-//    public class DocumentController : Controller
-//    {
-//        private IDocumentRepository _documentRepository;
+namespace MRZReader.Web.Controllers
+{
+    public class DocumentController : Controller
+    {
+        private IDocumentRepository _documentRepository;
 
-//        public DocumentController(IDocumentRepository documentRepository)
-//        {
-//            _documentRepository = documentRepository;
-//        }
-
-//        public IActionResult Index()
-//        {
-//            var model = _documentRepository.GetAll();
-//            return View(model);
-//        }
-//    }
-//}
+        public DocumentController(IDocumentRepository documentRepository)
+        {
+            _documentRepository = documentRepository;
+        }
+        public IActionResult Index()
+        {
+            var model = _documentRepository.GetAll().ToList().OrderBy(p => p.FileFullName);
+            return View(model);
+        }
+    }
+}
