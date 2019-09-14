fbf1a6196b86f4175bd70237feace37951f6951e
Mostafa Mahgoub <mostafa.mahgoub@belgiantrain.be>
2019-09-15 01:43:35 +0200

    Adding logging to MRZReader.DAL

--- MRZReader.Dal/DocumentRepository.cs
+++ MRZReader.Dal/DocumentRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using MRZReader.Core;
 
 namespace MRZReader.Dal
@@ -9,10 +10,12 @@ namespace MRZReader.Dal
     public class DocumentRepository : IDocumentRepository
     {
         private readonly MrzReaderDbContext _context;
+        private readonly ILogger _logger;
 
-        public DocumentRepository(MrzReaderDbContext context)
+        public DocumentRepository(ILogger<DocumentRepository> logger, MrzReaderDbContext context)
         {
             _context = context;
+            _logger = logger;
         }
         public DocumentRequest Add(DocumentRequest request)
         {
@@ -20,10 +23,8 @@ namespace MRZReader.Dal
             {
                 try
                 {
-                    request.Document.CreatedDate=DateTime.Now;
-
-
                     // Purpose of using transaction that we might want to add more things when we storing a document.
+                    request.Document.CreatedDate = DateTime.Now;
                     _context.Document.Add(request.Document);
                     _context.SaveChanges();
                     transaction.Commit();
@@ -32,13 +33,34 @@ namespace MRZReader.Dal
                 {
                     // No need to roll-back- automatic roll-back if something goes wrong
                     request.IsSuccessed = false;
+                    Log($"KO :Exception: {e.Message}", true);
+                    throw;
                 }
             }
             return request;
         }
         public IEnumerable<Document> GetAll()
         {
-           return _context.Document.Include("User").ToList();
+            try
+            {
+                return _context.Document.Include("User").ToList();
+            }
+            catch (Exception)
+            {
+                Log($"KO :Exception: {e.Message}", true);
+                throw;
+            }
+        }
+        private void Log(string msg, bool isEception = false)
+        {
+            if (isEception)
+            {
+                _logger.LogError($"[MRZ_Logs] {msg}.");
+            }
+            else
+            {
+                _logger.LogTrace($"[MRZ_Logs] {msg}.");
+            }
         }
     }
 }
