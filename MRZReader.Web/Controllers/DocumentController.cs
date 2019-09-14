5702dc07663befc9c3e2800ca8cc0f4e48ef961c
Mostafa Mahgoub <mostafa.mahgoub@belgiantrain.be>
2019-09-14 17:37:41 +0200

    File download

--- MRZReader.Web/Controllers/DocumentController.cs
+++ MRZReader.Web/Controllers/DocumentController.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MRZReader.Core;
 using ReflectionIT.Mvc.Paging;
@@ -17,6 +20,41 @@ namespace MRZReader.Web.Controllers
         {
             var model = _documentRepository.GetAll().ToList().OrderBy(p => p.FileFullName);
             return View(model);
+        }
+        public async Task<FileStreamResult> Download(string filename)
+        {
+            // Future Dev improvements, store the extension and mime type along with document in the database.
+            //var fileId=
+            //var fileName=
+            //var source = document.SourceFilePath;
+            //var target = document.TargetFilePath;
+            var path = filename;
+
+            var memory = new MemoryStream();
+            using (var stream = new FileStream(path, FileMode.Open))
+            {
+                await stream.CopyToAsync(memory);
+            }
+            memory.Position = 0;
+            return File(memory, GetContentType(path), Path.GetFileName(path));
+        }
+        private string GetContentType(string path)
+        {
+            var types = GetMimeTypes();
+            var ext = Path.GetExtension(path).ToLowerInvariant();
+            return types[ext];
+        }
+        private Dictionary<string, string> GetMimeTypes()
+        {
+            return new Dictionary<string, string>
+            {
+                {".xml", "application/xml"},
+                {".pdf", "application/pdf"},
+                {".png", "image/png"},
+                {".jpg", "image/jpeg"},
+                {".jpeg", "image/jpeg"},
+                {".gif", "image/gif"}
+            };
         }
     }
 }
